967c1ea915fc8f4d078c9f0a3e7142c2d262f7ec
Shiva <shivacr77@gmail.com>
2021-11-18 18:39:13 +0545

    pre-qualify form(step one) modified

---
+++ components/RegisterNav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const RegisterNavStyle = styled.div`
+  width: 100%;
+  background-color: #1b46b0;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+  line-height: 10px;
+ 
+  .reg-head p {
+    color: rgba(255, 255, 255, 0.7);
+    font-size: 16px;
+    line-height: 10px;
+  }
+`;
+function RegisterNav() {
+  return (
+    <RegisterNavStyle>
+      <section className="reg-head">
+        <h3>Soft Credit Check – No Upfront Fees – Apply Online</h3>
+        <p>Get started now by filling out the loan application below</p>
+      </section>
+    </RegisterNavStyle>
+  );
+}
+
+export default RegisterNav;
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 import { CookiesProvider } from "react-cookie";
 
 function MyApp({ Component, pageProps }) {
   return (
     <CookiesProvider>
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    
     </CookiesProvider>
-    
   );
 }
 
