b352fb53aed2975ce385ab8124e6612be4a589fa
anish98166 <anish98166@gmail.com>
2021-11-19 13:15:34 +0545

    form 4 validation added

--- pages/form4.js
+++ pages/form4.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 
 const Hero = styled.div`
 display: flex;
@@ -229,6 +230,13 @@ input[type=submit]{
 
 
 export default function Form() {
+
+  const { register, handleSubmit, error } = useForm();
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  }
+
   return (
     <>
       <Head>
@@ -237,7 +245,7 @@ export default function Form() {
       </Head>
       <Hero>
 
-        <form className="formstyle" action="form6">
+        <form className="formstyle" action="form6" onSubmit={handleSubmit(onSubmit)}>
 
           <section className="Form-design">
 
@@ -250,55 +258,113 @@ export default function Form() {
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Full Name</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="John" required />
+                <input
+                  {...register("fullname", {
+                    required: "true",
+                    maxLength: {
+                      value: 20,
+                      message: "max length is 25"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="John" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">Date of Birth</label>
-                <input id="firstname" className="textbox" type="date" autoComplete="fdba" placeholder="Enter Monthly Total Business Expenses" required />
+                <input
+                  {...register("dateofbirth", {
+                    required: "true",
+                  })}
+                  id="firstname" className="textbox" type="date" autoComplete="fdba" placeholder="Enter Monthly Total Business Expenses" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Home Address</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Address" required />
+                <input
+                  {...register("homeaddress", {
+                    required: "true",
+
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Address" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">City</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="Enter City" required />
+                <input
+                  {...register("city", {
+                    required: "true",
+
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="Enter City" required />
               </div>
             </div>
 
             <div className="form-row-three form-gap">
               <div className="form-group form-city">
                 <label htmlFor="fname" className="formlabel">State</label>
-                <input id="city" className="textbox" type="state" autoComplete="fname" placeholder="Select State" required />
+                <input
+                  {...register("state", {
+                    required: "true",
+
+                  })}
+                  id="city" className="textbox" type="state" autoComplete="fname" placeholder="Select State" required />
               </div>
               <div className="form-group form-state">
                 <label htmlFor="fname" className="formlabel">Zip Code</label>
-                <input id="state" className="textbox" type="text" autoComplete="fname" placeholder="Enter Zip Code" required />
+                <input
+                  {...register("zipcode", {
+                    required: "true",
+
+                  })}
+                  id="state" className="textbox" type="number" autoComplete="fname" placeholder="Enter Zip Code" required />
               </div>
               <div className="form-group form-zip">
                 <label htmlFor="fname" className="formlabel">Social Security Number</label>
-                <input id="zipcode" className="textbox" type="number" autoComplete="fname" placeholder="Social Security Number" required />
+                <input
+                  {...register("socialsecuritynumber", {
+                    required: "true",
+
+                  })}
+                  id="zipcode" className="textbox" type="number" autoComplete="fname" placeholder="Social Security Number" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Email</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Email" required />
+                <input
+                  {...register("email", {
+                    required: "true",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "Entered value does not match email format"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Email" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">Mobile</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="(XXX)-(XXX)-(XXXX)" required />
+                <input
+                  {...register("mobile", {
+                    required: "true",
+                    maxLength: {
+                      value: 15,
+                      message: "max length is 15"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="number" autoComplete="fdba" placeholder="(XXX)-(XXX)-(XXXX)" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Ownership</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter percent of ownership" required />
+                <input
+                  {...register("ownership", {
+                    required: "true",
+
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter percent of ownership" required />
               </div>
 
             </div>
@@ -308,17 +374,29 @@ export default function Form() {
               <div className="radio-three">
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="own-click" />
+                  <input
+                    {...register("citizen", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="own-click" />
                   <label>US Citizen</label>
                 </div>
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="mortgage-click" />
+                  <input
+                    {...register("citizen", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="mortgage-click" />
                   <label>US Permanent Resident</label>
                 </div>
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="rent-click" />
+                  <input
+                    {...register("citizen", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="rent-click" />
                   <label>Other</label>
                 </div>
 
@@ -337,55 +415,112 @@ export default function Form() {
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Full Name</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="John" required />
+                <input
+                  {...register("fullname", {
+                    required: "true",
+                    maxLength: {
+                      value: 20,
+                      message: "max length is 25"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="John" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">Date of Birth</label>
-                <input id="firstname" className="textbox" type="date" autoComplete="fdba" placeholder="Enter Monthly Total Business Expenses" required />
+                <input
+                  {...register("dateofbirth", {
+                    required: "true",
+                  })}
+                  id="firstname" className="textbox" type="date" autoComplete="fdba" placeholder="Enter Monthly Total Business Expenses" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Home Address</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Address" required />
+                <input
+                  {...register("homeaddress", {
+                    required: "true",
+
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Address" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">City</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="Enter City" required />
+                <input
+                  {...register("city", {
+                    required: "true",
+
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="Enter City" required />
               </div>
             </div>
 
             <div className="form-row-three form-gap">
               <div className="form-group form-city">
                 <label htmlFor="fname" className="formlabel">State</label>
-                <input id="city" className="textbox" type="state" autoComplete="fname" placeholder="Select State" required />
+                <input
+                  {...register("state", {
+                    required: "true",
+
+                  })}
+                  id="city" className="textbox" type="state" autoComplete="fname" placeholder="Select State" required />
               </div>
               <div className="form-group form-state">
                 <label htmlFor="fname" className="formlabel">Zip Code</label>
-                <input id="state" className="textbox" type="text" autoComplete="fname" placeholder="Enter Zip Code" required />
+                <input
+                  {...register("zipcode", {
+                    required: "true",
+
+                  })}
+                  id="state" className="textbox" type="number" autoComplete="fname" placeholder="Enter Zip Code" required />
               </div>
               <div className="form-group form-zip">
                 <label htmlFor="fname" className="formlabel">Social Security Number</label>
-                <input id="zipcode" className="textbox" type="number" autoComplete="fname" placeholder="Social Security Number" required />
+                <input
+                  {...register("socialsecuritynumber", {
+                    required: "true",
+
+                  })}
+                  id="zipcode" className="textbox" type="number" autoComplete="fname" placeholder="Social Security Number" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Email</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Email" required />
+                <input
+                  {...register("email", {
+                    required: "true",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "Entered value does not match email format"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter Email" required />
               </div>
               <div className="form-group form-dba">
                 <label htmlFor="fdba" className="formlabel">Mobile</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fdba" placeholder="(XXX)-(XXX)-(XXXX)" required />
+                <input
+                  {...register("mobile", {
+                    required: "true",
+                    maxLength: {
+                      value: 15,
+                      message: "max length is 15"
+                    }
+                  })}
+                  id="firstname" className="textbox" type="number" autoComplete="fdba" placeholder="(XXX)-(XXX)-(XXXX)" required />
               </div>
             </div>
 
             <div className="form-row-one form-gap">
               <div className="form-group form-name">
                 <label htmlFor="fname" className="formlabel ">Ownership</label>
-                <input id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter percent of ownership" required />
+                <input
+                  {...register("ownership", {
+                    required: "true",
+                  })}
+                  id="firstname" className="textbox" type="text" autoComplete="fname" placeholder="Enter percent of ownership" required />
               </div>
 
             </div>
@@ -395,17 +530,29 @@ export default function Form() {
               <div className="radio-three">
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="own-click" />
+                  <input
+                    {...register("citizen1", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="own-click" />
                   <label>US Citizen</label>
                 </div>
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="mortgage-click" />
+                  <input
+                    {...register("citizen1", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="mortgage-click" />
                   <label>US Permanent Resident</label>
                 </div>
 
                 <div className="radio-container">
-                  <input type="radio" name="radio" className="rent-click" />
+                  <input
+                    {...register("citizen1", {
+                      required: "true",
+                    })}
+                    type="radio" name="radio" className="rent-click" />
                   <label>Other</label>
                 </div>
 
